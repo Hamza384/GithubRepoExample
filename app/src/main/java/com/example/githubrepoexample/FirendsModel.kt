5fd5b6b079d131d810541fe8a819ab654a889edb
Hamza <hamzaciit133@gmail.com>
2021-10-22 10:33:05 +0500

    Added Model Varaible in it from Friend

--- app/src/main/java/com/example/githubrepoexample/FirendsModel.kt
+++ app/src/main/java/com/example/githubrepoexample/FirendsModel.kt
@@ -1,5 +1,8 @@
 package com.example.githubrepoexample
 
 class FirendsModel {
+    var name : String = "Hamza Arshad"
+    var age: Int = 24
+    var phNumber : Int = 0
 
 }
--- app/src/main/java/com/example/githubrepoexample/MainActivity.kt
+++ app/src/main/java/com/example/githubrepoexample/MainActivity.kt
@@ -7,8 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        
         println("First Commit")
         println("Second Commit from friend")
 
